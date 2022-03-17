873a825151cd7609ffa1bbef8a63211e439c21f5
M.M <mmendes090@live.com>
2022-03-17 10:13:25 -0300

    Configure postgres engine.

--- pythonapp/settings.py
+++ pythonapp/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'pythonapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
